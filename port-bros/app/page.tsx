7a10ba32f40caab7a480360d6123c2ac6d8a0987
mcxpm <markuslec@gmail.com>
2023-10-01 14:59:42 +0800

    finalised login

--- port-bros/app/page.tsx
+++ port-bros/app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { options } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   const session = await getServerSession(options);
-  return <div>Home</div>;
+  const usr = await prisma.user.findFirst({
+    where: {
+      email: "test@test.com",
+    },
+  });
+  return (
+    <main>
+      <div>Hello, {usr?.name} </div>
+    </main>
+  );
 }
